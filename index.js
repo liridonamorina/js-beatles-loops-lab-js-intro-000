20d224e76718adf069197d25c10549eadb84fcd4
Liridona Morina <liridonamorina00@gmail.com>
2020-02-18 14:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay(arrayOfM, arrayOfI)
+{
+  var newArray = [];
+  let string = ""
+  for(let i = 0; i < arrayOfM.length; i++)
+  {
+    string = `${arrayOfM[i]} plays ${arrayOfI[i]}`;
+    newArray.push(string);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(arrayOfFacts)
+{
+  var i = 0;
+  let string = "";
+  while(i < arrayOfFacts.length ){
+    string = `${arrayOfFacts[i]}!!!`;
+    i++;
+  }
+  return string;
+}
+
+function iLoveTheBeatles(num){
+
+  var newArray = [];
+  var string = "";
+  do {
+    string = "I love Beatles!";
+    i++;
+  }
+  while(i < num){
+    newArray.push(string)
+  }
+
+}
