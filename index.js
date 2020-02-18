1eaa40f2e7eaa063d1c593a7c4f398365fb1310f
Liridona Morina <liridonamorina00@gmail.com>
2020-02-18 15:06:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+function theBeatlesPlay(arrayOfM, arrayOfI)
+{
+  var newArray = [];
+  let string = ""
+  for(let i = 0; i < arrayOfM.length; i++)
+  {
+    string = `${arrayOfM[i]} plays ${arrayOfI[i]}`;
+    newArray.push(string);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(arrayOfFacts)
+{
+  let string = "";
+  var newArray = [];
+  var i = 0;
+  while(i < arrayOfFacts.length ){
+    string = `${arrayOfFacts[i]}!!!`;
+    newArray.push(string);
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(num){
+
+  var newArray = [];
+  var string = "";
+
+  do {
+    string = "I love the Beatles!";
+    newArray.push(string)
+    num++;
+  }
+  while(num < 15){
+  return newArray;
+  }
+}
